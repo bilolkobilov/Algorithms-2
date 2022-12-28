1e675674043775e5858396b1b6f67d26b8ee8a8b
Bilol Kobilov <115587177+1bilol@users.noreply.github.com>
2022-12-28 13:47:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3_classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Bilol", 18, "langar 2", "qobilovbilol8222@gmail.com");
+
+            Console.WriteLine(student1.name);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3_classes
+{
+    internal class Student
+    {
+        public string name;
+        public int age;
+        public string addres;
+        public string eaddres;
+
+        public Student(string aName, int aAge, string aAddres, string aEaddres)
+        {
+            name = aName;
+            age = aAge;
+            addres = aAddres;
+            eaddres = aEaddres;
+
+        }
+    }
+}
