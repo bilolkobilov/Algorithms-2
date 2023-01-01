8939ead1a738153f816c90d7bb68aa1388d9504f
Bilol Kobilov <115587177+1bilol@users.noreply.github.com>
2023-01-01 12:15:29 +0300

    Add files via upload

---
+++ fanal/Class3.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace homework3_classes3_
+{
+    class Class3
+    {
+        public class Student
+        {
+            private String name;
+            private int age;
+            private String address;
+            private String email;
+
+            public Student(String Name, int Age, String Address, String Email)
+            {
+                name = Name;
+                age = Age;
+                address = Address;
+                email = Email;
+                if (email.Contains("gmail.com"))
+                {
+                    
+                }
+            }
+            public String Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    name = value;
+                }
+            }
+            public int Age
+            {
+                get
+                {
+                    return age;
+                }
+                set
+                {
+                    age = value;
+                }
+            }
+            public string Addres
+            {
+                get
+                {
+                    return address;
+                }
+                set
+                {
+                    address = value;
+                }
+            }
+            public string Email
+            {
+                get
+                {
+                    return email;
+                }
+                set
+                {
+                    email = value;
+                }
+            }
+            public override string ToString()
+            {
+                return "Name-" + name + ",  Age-" + age + ",  Address-" + address + ",  Email-" + email + ".";
+               
+
+            }
+           
+        }
+    }
+}  
---
+++ fanal/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static homework3_classes3_.Class3;
+
+namespace homework3_classes3_
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Student[] student1 = new Student[2];
+           
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write("Name: ");
+                string name = Convert.ToString(Console.ReadLine());
+                Console.Write("Age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Address: ");
+                string address = Convert.ToString(Console.ReadLine());
+                Console.Write("Email: ");
+                string email = Convert.ToString(Console.ReadLine());
+                student1[i] = new Student(name, age, address, email);
+                student1[i] = new Student(name, age, address, email);
+            }
+           
+
+            Console.WriteLine(student1[0]);
+            Console.WriteLine(student1[1]);
+
+            Console.ReadLine();
+
+        }
+    }
+}
